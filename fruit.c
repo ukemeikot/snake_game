47838f1cf1e99df2a2f2a4337e4e9a9d4bb36806
SundayMba <sundinoh@gmail.com>
2023-11-22 14:49:40 +0100

    added srand and time func, and create array for body and head

--- fruit.c
+++ fruit.c
@@ -6,22 +6,32 @@
 
 void setup(void)
 {
+	srand(time(0));
 	initscr();
 	noecho();
 	curs_set(0);
 	keypad(stdscr, TRUE);
 	timeout(0);
+	int count;
+	point body;
 
 	getmaxyx(stdscr, max_y, max_x);
 
 	game_over = 0;
+	count = 3;
 	score = 0;
 
-	snake_length = 1;
+	snake_length = 3;
 
 	/*Snake starting position*/
 	x = max_x / 2;
 	y = max_y / 2;
+	while (count)
+	{
+		point body = {.x = x - (count + 5), .y = y};
+		count--;
+		snake_body[count] = body;
+	}
 
 	direction = KEY_RIGHT;
 
--- main.h
+++ main.h
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ncurses.h>
+#include <time.h>
 extern int fruit_x, fruit_y, x, y, game_over, flag, score, max_x, max_y;
 extern int snake_length, direction;
 /*User defined Function prototype*/
@@ -16,6 +17,14 @@ void input(void);
 void algorithm(void);
 void setup(void);
 /*User Define Variables*/
+
+typedef struct point {
+	int x;
+	int y;
+} point;
+
+extern point snake_body[100];
+extern point head;
 
 #endif
 
